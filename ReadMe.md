076e0b348585e84a404dabb0efbee37fec708cc6
drexelapplab <applab@drexel.edu>
2020-07-27 13:26:45 -0400

    first commit
    first commit

---
+++ ReadMe.md
@@ -0,0 +1,5 @@
+Basic WatchOS App
+
+using ChrisLearns WatchOS timer app tutorial as a base:
+channel: https://www.youtube.com/channel/UC8lyFZnkzaRA61ZZIEgWf6w
+video: https://www.youtube.com/watch?v=PnAvYJGNTPQ
--- WatchOS_timerBasics WatchKit Extension/ContentView.swift
+++ WatchOS_timerBasics WatchKit Extension/ContentView.swift
@@ -9,11 +9,63 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var secondScreenShown = false
+    @State var timerVal = 1
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack{
+            Text("Start timer for \(timerVal) seconds")
+                .font(.system(size: 14))
+            Picker(selection: $timerVal, label: Text("")) {
+                /*@START_MENU_TOKEN@*/Text("1").tag(1)/*@END_MENU_TOKEN@*/
+                Text("5").tag(5)
+                Text("10").tag(10)
+                Text("15").tag(15)
+
+            }
+            NavigationLink(destination: SecondView(secondScreenShown: $secondScreenShown, timerVal: timerVal), isActive: $secondScreenShown, label: {Text("Go")})
+        }
     }
 }
 
+struct SecondView: View{
+    @Binding var secondScreenShown:Bool
+    @State var timerVal:Int
+    var body: some View {
+        VStack{
+            if timerVal > 0 {
+                Text("Time remaining")
+                    .font(.system(size: 14))
+                Text("\(timerVal)")
+                    .font(.system(size: 40))
+                    .onAppear(){
+                        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) {_ in
+                            if self.timerVal > 0 {
+                                self.timerVal -= 1
+                            }
+                        }
+                }
+                Text("seconds")
+                    .font(.system(size: 14))
+                Button(action: {self.secondScreenShown = false
+                    }) {
+                    Text("Cancel")
+                        .foregroundColor(.red)
+                }
+            } else {
+                Button(action: {
+                    self.secondScreenShown = false
+                }) {
+                    Text("Done")
+                        .foregroundColor(.green)
+                }
+            }
+
+        }
+        
+    }
+    
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
